67a9a9dbd6a926cdf537e206e972006ae118c3ef
Daujard <valmon.daujard@gmail.com>
2020-04-09 15:11:34 +0200

    Test

--- tp-vol-spring/src/test/java/tp/Test.java
+++ tp-vol-spring/src/test/java/tp/Test.java
@@ -4,7 +4,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import tp.Application;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import tp.model.Aeroport;
 import tp.model.Arrivee;
 import tp.model.Avion;
@@ -19,41 +20,69 @@ import tp.model.Trajet;
 import tp.model.Utilisateur;
 import tp.model.Ville;
 import tp.model.Vol;
+import tp.model.persistence.IAeroportDao;
+import tp.model.persistence.IArriveeDao;
+import tp.model.persistence.IAvionDao;
+import tp.model.persistence.IClientDao;
+import tp.model.persistence.ICompagnieDao;
+import tp.model.persistence.IDepartDao;
 import tp.model.persistence.IPaiementDao;
+import tp.model.persistence.IParticulierDao;
 import tp.model.persistence.IPassagerDao;
 import tp.model.persistence.IReservationDao;
+import tp.model.persistence.ISocieteDao;
 import tp.model.persistence.ITrajetDao;
+import tp.model.persistence.IUtilisateurDao;
+import tp.model.persistence.IVilleDao;
+import tp.model.persistence.IVolDao;
 
 public class Test {
 
 	public static void main(String[] args) throws ParseException {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
 
-		IPaiementDao testPaiement = Application.getInstance().getPaiementDao();
-		IReservationDao testReservation = Application.getInstance().getReservationDao();
-		ITrajetDao testTrajet = Application.getInstance().getTrajetDao();
-		IPassagerDao testPassager = Application.getInstance().getPassagerDao();
+		IPaiementDao testPaiement = context.getBean(IPaiementDao.class);
+		IReservationDao testReservation = context.getBean(IReservationDao.class);
+		ITrajetDao testTrajet = context.getBean(ITrajetDao.class);
+		ICompagnieDao testCompagnie = context.getBean(ICompagnieDao.class);
+		IPassagerDao testPassager = context.getBean(IPassagerDao.class);
+		ISocieteDao testSociete = context.getBean(ISocieteDao.class);
+		IAeroportDao testAeroport = context.getBean(IAeroportDao.class);
+		IVilleDao testVille = context.getBean(IVilleDao.class);
+		IAvionDao testAvion = context.getBean(IAvionDao.class);
+		IVolDao testVol = context.getBean(IVolDao.class);
+		IDepartDao testDepart = context.getBean(IDepartDao.class);
+		IParticulierDao testParticulier = context.getBean(IParticulierDao.class);
+		IClientDao testClient = context.getBean(IClientDao.class);
+		IUtilisateurDao testUtilisateur = context.getBean(IUtilisateurDao.class);
+		IArriveeDao testArrivee = context.getBean(IArriveeDao.class);
+		
+		
+		
+		
 		
 		
 		Compagnie airFrance = new Compagnie("Air France");
-		airFrance = Application.getInstance().getCompagnieDao().save(airFrance);
+		airFrance = testCompagnie.save(airFrance);
 		
 			Avion airbusA380 = new Avion("Airbus A380");
-			airbusA380 = Application.getInstance().getAvionDao().save(airbusA380);
+			airbusA380 =testAvion.save(airbusA380);
 		
 				Ville paris = new Ville("Paris");
-				paris = Application.getInstance().getVilleDao().save(paris);
+				paris =testVille.save(paris);
 				Ville bordeaux = new Ville("Bordeaux");
-				bordeaux = Application.getInstance().getVilleDao().save(bordeaux);
+				bordeaux = testVille.save(bordeaux);
 		
 			Aeroport roissy = new Aeroport("Aeroport de Roissy");
-			roissy = Application.getInstance().getAeroportDao().save(roissy);
+			roissy = testAeroport.save(roissy);
 			Aeroport bordeauxAeroport = new Aeroport("Aeroport de Bordeaux");
-			bordeauxAeroport = Application.getInstance().getAeroportDao().save(bordeauxAeroport);
+			bordeauxAeroport = testAeroport.save(bordeauxAeroport);
 		
 			Vol bordeauxParis = new Vol("AF4578");
-			bordeauxParis = Application.getInstance().getVolDao().save(bordeauxParis);
+			bordeauxParis = testVol.save(bordeauxParis);
 			
 			bordeauxParis.setCompagnie(airFrance);
 //			airFrance.addVol(bordeauxParis);
@@ -63,11 +92,11 @@ public class Test {
 
 			
 			Depart bordeauxParisDepart = new Depart(new Date());
-			bordeauxParisDepart = Application.getInstance().getDepartDao().save(bordeauxParisDepart);
+			bordeauxParisDepart = testDepart.save(bordeauxParisDepart);
 			bordeauxParis.setDepart(bordeauxParisDepart);
 			
 			Arrivee bordeauxParisArrivee = new Arrivee(new Date());
-			bordeauxParisArrivee = Application.getInstance().getArriveeDao().save(bordeauxParisArrivee);
+			bordeauxParisArrivee = testArrivee.save(bordeauxParisArrivee);
 			bordeauxParis.setArrivee(bordeauxParisArrivee);
 			
 			bordeauxParisArrivee.setAeroport(roissy);
@@ -77,11 +106,11 @@ public class Test {
 			bordeauxAeroport.addVille(bordeaux);
 			roissy.addVille(paris);
 			
-			bordeauxParisDepart = Application.getInstance().getDepartDao().save(bordeauxParisDepart);
-			bordeauxParisArrivee = Application.getInstance().getArriveeDao().save(bordeauxParisArrivee);
-			roissy = Application.getInstance().getAeroportDao().save(roissy);
-			bordeauxAeroport = Application.getInstance().getAeroportDao().save(bordeauxAeroport);
-			bordeauxParis = Application.getInstance().getVolDao().save(bordeauxParis);
+			bordeauxParisDepart = testDepart.save(bordeauxParisDepart);
+			bordeauxParisArrivee = testArrivee.save(bordeauxParisArrivee);
+			roissy = testAeroport.save(roissy);
+			bordeauxAeroport = testAeroport.save(bordeauxAeroport);
+			bordeauxParis = testVol.save(bordeauxParis);
 	
 		
 		Societe sopraSteria = new Societe("Societe", "SOPRA",  "Numero de siret", "Numero de TVA", "SopraMail", "Tel Sopra", 20, "Avenue Pythagore", null, 33700,
@@ -90,8 +119,8 @@ public class Test {
 		Particulier eric = new Particulier("Particulier", "SULTAN", "Eric", "e.sultan@ajc-ingenierie.fr", "0645104506", 86, "Avenue JFK", null, 33700,
 				"MERIGNAC", "FRANCE");
 		
-		sopraSteria = Application.getInstance().getSocieteDao().save(sopraSteria);
-		eric = Application.getInstance().getParticulierDao().save(eric);
+		sopraSteria = testSociete.save(sopraSteria);
+		eric = testParticulier.save(eric);
 
 		System.out.println(sopraSteria);
 		System.out.println(eric);
@@ -99,18 +128,18 @@ public class Test {
 		Utilisateur userSopra = new Utilisateur("id SOPRA", "mdp");
 		Utilisateur userEric = new Utilisateur("id Eric", "MDP");
 
-		userSopra = Application.getInstance().getUtilisateurDao().save(userSopra);
-		userEric = Application.getInstance().getUtilisateurDao().save(userEric);
+		userSopra = testUtilisateur.save(userSopra);
+		userEric = testUtilisateur.save(userEric);
 		
 		sopraSteria.setUtilisateur(userSopra);
 		userSopra.setClient(sopraSteria);
 		eric.setUtilisateur(userEric);
 		userEric.setClient(eric);
 		
-		sopraSteria = Application.getInstance().getSocieteDao().save(sopraSteria);
-		userSopra = Application.getInstance().getUtilisateurDao().save(userSopra);
-		eric = Application.getInstance().getParticulierDao().save(eric);
-		userEric = Application.getInstance().getUtilisateurDao().save(userEric);
+		sopraSteria = testSociete.save(sopraSteria);
+		userSopra = testUtilisateur.save(userSopra);
+		eric = testParticulier.save(eric);
+		userEric = testUtilisateur.save(userEric);
 		
 		
 		Paiement paiement1 = new Paiement();
@@ -140,7 +169,7 @@ public class Test {
 		parisNY = testTrajet.save(parisNY);
 		bordeauxParis.addTrajet(parisNY);
 		
-		bordeauxParis = Application.getInstance().getVolDao().save(bordeauxParis);
+		bordeauxParis = testVol.save(bordeauxParis);
 		reservation1.setTrajet(parisNY);
 		
 		reservation1 = testReservation.save(reservation1);
